739dffdf35c87c01ddc6803ebb796c72d479c549
Brandon Yuong <brandonyuong@gmail.com>
2018-08-23 15:15:33 -0700

    Implement addChild method for GenericTree class.

--- src/GenericTree.java
+++ src/GenericTree.java
@@ -44,4 +44,32 @@ public class GenericTree<E> implements Cloneable
     {
         traverse(func, mRoot, 0);
     }
+
+    public TreeNode<E> addChild( TreeNode<E> treeNode,  E x )
+    {
+        // empty tree? - create a root node if user passes in null
+        if (mSize == 0)
+        {
+            if (treeNode != null)
+                return null; // error something's fishy.  treeNode can't right
+            mRoot = new TreeNode<E>(x, null, null, null);
+            mRoot.myRoot = mRoot;
+            mSize = 1;
+            return mRoot;
+        }
+        if (treeNode == null)
+            return null; // error inserting into non_null tree with a null parent
+        if (treeNode.myRoot != mRoot)
+            return null;  // silent error, node does not belong to this tree
+
+        // push this node into the head of the sibling list; adjust prev pointers
+        TreeNode<E> newNode = new TreeNode<E>(x,
+                treeNode.firstChild, null, treeNode, mRoot);  // sb, chld, prv, rt
+        treeNode.firstChild = newNode;
+        if (newNode.sib != null)
+            newNode.sib.prev = newNode;
+        ++mSize;
+        return newNode;
+    }
+
 }
