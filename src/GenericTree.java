fc7c8376d2a18c0dae2d306088dc2df464419ee6
Brandon Yuong <brandonyuong@gmail.com>
2018-08-23 16:07:34 -0700

    Implement clone() for GenericTree class.

--- src/GenericTree.java
+++ src/GenericTree.java
@@ -106,6 +106,70 @@ public class GenericTree<E> implements Cloneable
 
     private void removeNode(TreeNode<E> nodeToDelete )
     {
-        
+        if (nodeToDelete == null || mRoot == null)
+            return;
+        if (nodeToDelete.myRoot != mRoot)
+            return;  // silent error, node does not belong to this tree
+
+        // remove all the children of this node
+        while (nodeToDelete.firstChild != null)
+            removeNode(nodeToDelete.firstChild);
+
+        if (nodeToDelete.prev == null)
+            mRoot = null;  // last node in tree
+        else if (nodeToDelete.prev.sib == nodeToDelete)
+            nodeToDelete.prev.sib = nodeToDelete.sib; // adjust left sibling
+        else
+            nodeToDelete.prev.firstChild = nodeToDelete.sib;  // adjust parent
+
+        // adjust the successor sib's prev pointer
+        if (nodeToDelete.sib != null)
+            nodeToDelete.sib.prev = nodeToDelete.prev;
     }
+
+    public Object clone() throws CloneNotSupportedException
+    {
+        GenericTree<E> newObject = (GenericTree<E>)super.clone();
+        newObject.clear();  // can't point to other's data
+
+        newObject.mRoot = cloneSubGenericTree(mRoot);
+        newObject.mSize = mSize;
+        newObject.setMyRoots(newObject.mRoot);
+
+        return newObject;
+    }
+
+    private TreeNode<E> cloneSubGenericTree(TreeNode<E> root)
+    {
+        TreeNode<E> newNode;
+        if (root == null)
+            return null;
+
+        // does not set myRoot which must be done by caller
+        newNode = new TreeNode<E>
+                (
+                        root.data,
+                        cloneSubGenericTree(root.sib), cloneSubGenericTree(root.firstChild),
+                        null
+                );
+
+        // the prev pointer is set by parent recursive call ... this is the code:
+        if (newNode.sib != null)
+            newNode.sib.prev = newNode;
+        if (newNode.firstChild != null)
+            newNode.firstChild.prev = newNode;
+        return newNode;
+    }
+
+    // recursively sets all myRoots to mRoot
+    private void setMyRoots(TreeNode<E> treeNode)
+    {
+        if (treeNode == null)
+            return;
+
+        treeNode.myRoot = mRoot;
+        setMyRoots(treeNode.sib);
+        setMyRoots(treeNode.firstChild);
+    }
+
 }
